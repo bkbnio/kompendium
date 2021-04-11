98d29f2d53a55177571fd08632d764e72c1546b3
Ryan <ryanbrink@pm.me>
2021-04-11 09:52:26 -0400

    playground running

--- playground/build.gradle.kts
+++ playground/build.gradle.kts
@@ -5,6 +5,18 @@ plugins {
 dependencies {
   implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+  implementation(projects.lib)
+
+  implementation(libs.bundles.ktor)
+  implementation(libs.bundles.logging)
+
   testImplementation("org.jetbrains.kotlin:kotlin-test")
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+application {
+  @Suppress("DEPRECATION")
+  mainClassName = "org.leafygreens.kompendium.playground.MainKt"
+  applicationDefaultJvmArgs = listOf("-Dio.ktor.development=true") // TODO I don't think this is working 😢
 }
---
+++ playground/src/main/kotlin/org/leafygreens/kompendium/playground/Main.kt
@@ -0,0 +1,28 @@
+package org.leafygreens.kompendium.playground
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.route
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main() {
+  embeddedServer(
+    Netty,
+    port = 8080,
+    module = Application::mainModule
+  ).start(wait = true)
+}
+
+fun Application.mainModule() {
+  routing {
+    route("/") {
+      get {
+        call.respondText("hi")
+      }
+    }
+  }
+}
