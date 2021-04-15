66e3fe5aa63cd6329a45c56cdbdcbfee34b56b3d
Ryan <ryanbrink@pm.me>
2021-04-15 17:46:41 -0400

    udpated readme, dropped old annotation

--- kompendium-core/src/main/kotlin/org/leafygreens/kompendium/annotations/KompendiumLicense.kt
+++
@@ -1,8 +0,0 @@
-package org.leafygreens.kompendium.annotations
-
-@Retention(AnnotationRetention.SOURCE)
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
-annotation class KompendiumLicense(
-  val name: String,
-  val url: String = ""
-)
