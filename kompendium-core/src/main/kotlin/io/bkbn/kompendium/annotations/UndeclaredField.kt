aeff1b0455f6de6d4648b44c283c48eaea30f92e
Ryan Brink <rbweb@protonmail.com>
2021-08-14 20:00:44 -0500

    limit to runtime

--- kompendium-core/src/main/kotlin/io/bkbn/kompendium/annotations/UndeclaredField.kt
+++ kompendium-core/src/main/kotlin/io/bkbn/kompendium/annotations/UndeclaredField.kt
@@ -2,7 +2,7 @@ package io.bkbn.kompendium.annotations
 
 import kotlin.reflect.KClass
 
-//@Retention(AnnotationRetention.RUNTIME)
+@Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.CLASS)
 @Repeatable
 annotation class UndeclaredField(val field: String, val clazz: KClass<*>)
