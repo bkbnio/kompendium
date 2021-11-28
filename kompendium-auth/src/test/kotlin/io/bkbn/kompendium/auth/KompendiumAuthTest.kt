cac47d4ff3fed35f726d51df4c42d13efcc0f20e
Ryan Brink <rbweb@pm.me>
2021-11-28 11:05:50 -0500

    sparkly

--- kompendium-auth/src/test/kotlin/io/bkbn/kompendium/auth/KompendiumAuthTest.kt
+++ kompendium-auth/src/test/kotlin/io/bkbn/kompendium/auth/KompendiumAuthTest.kt
@@ -7,60 +7,41 @@ import io.bkbn.kompendium.auth.util.configJwtAuth
 import io.bkbn.kompendium.auth.util.notarizedAuthenticatedGetModule
 import io.bkbn.kompendium.auth.util.setupOauth
 import io.bkbn.kompendium.core.Kompendium
-import io.bkbn.kompendium.core.TestHelpers.compareOpenAPISpec
-import io.bkbn.kompendium.core.docs
-import io.bkbn.kompendium.core.jacksonConfigModule
+import io.bkbn.kompendium.core.TestHelpers.openApiTest
 import io.bkbn.kompendium.oas.security.OAuth
 import io.kotest.core.spec.style.DescribeSpec
 import io.ktor.application.install
 import io.ktor.auth.Authentication
-import io.ktor.server.testing.withTestApplication
 
 class KompendiumAuthTest : DescribeSpec({
   afterEach { Kompendium.resetSchema() }
   describe("Basic Authentication") {
     it("Can create a notarized basic authentication record with all expected information") {
-      // arrange
-      withTestApplication({
-        jacksonConfigModule()
-        docs()
+      // act
+      openApiTest("notarized_basic_authenticated_get.json") {
         configBasicAuth()
         notarizedAuthenticatedGetModule(AuthConfigName.Basic)
-      }) {
-        // act
-        compareOpenAPISpec("notarized_basic_authenticated_get.json")
       }
     }
   }
   describe("JWT Authentication") {
     it("Can create a notarized jwt authentication record with all expected information") {
-      // arrange
-      withTestApplication({
-        jacksonConfigModule()
+      // act
+      openApiTest("notarized_jwt_authenticated_get.json") {
         configJwtAuth()
-        docs()
         notarizedAuthenticatedGetModule(AuthConfigName.JWT)
-      }) {
-        // act
-        compareOpenAPISpec("notarized_jwt_authenticated_get.json")
       }
     }
     it("Can create a JWT authentication record with a custom scheme") {
-      // arrange
-      withTestApplication({
-        jacksonConfigModule()
+      // act
+      openApiTest("notarized_jwt_custom_scheme_authenticated_get.json") {
         configJwtAuth(bearerFormat = "JWT")
-        docs()
         notarizedAuthenticatedGetModule(AuthConfigName.JWT)
-      }) {
-        // act
-        compareOpenAPISpec("notarized_jwt_custom_scheme_authenticated_get.json")
       }
     }
     it("Can create a JWT authentication record with multiple schemas") {
-      // arrange
-      withTestApplication({
-        jacksonConfigModule()
+      // act
+      openApiTest("notarized_multiple_jwt_authenticated_get.json") {
         install(Authentication) {
           notarizedJwt("jwt1") {
             realm = "Ktor server"
@@ -69,11 +50,7 @@ class KompendiumAuthTest : DescribeSpec({
             realm = "Ktor server"
           }
         }
-        docs()
         notarizedAuthenticatedGetModule("jwt1", "jwt2")
-      }) {
-        // act
-        compareOpenAPISpec("notarized_multiple_jwt_authenticated_get.json")
       }
     }
   }
@@ -90,14 +67,10 @@ class KompendiumAuthTest : DescribeSpec({
         clientCredentials = OAuth.Flows.ClientCredential("https://accounts.google.com/token")
       )
 
-      withTestApplication({
-        jacksonConfigModule()
+      // act
+      openApiTest("notarized_oauth_implicit_flow.json") {
         setupOauth(flows)
-        docs()
         notarizedAuthenticatedGetModule("oauth")
-      }) {
-        // act
-        compareOpenAPISpec("notarized_oauth_implicit_flow.json")
       }
     }
   }
--- kompendium-locations/src/test/kotlin/io/bkbn/kompendium/locations/KompendiumLocationsTest.kt
+++ kompendium-locations/src/test/kotlin/io/bkbn/kompendium/locations/KompendiumLocationsTest.kt
@@ -2,6 +2,7 @@ package io.bkbn.kompendium.locations
 
 import io.bkbn.kompendium.core.Kompendium
 import io.bkbn.kompendium.core.TestHelpers.compareOpenAPISpec
+import io.bkbn.kompendium.core.TestHelpers.openApiTest
 import io.bkbn.kompendium.core.docs
 import io.bkbn.kompendium.core.jacksonConfigModule
 import io.bkbn.kompendium.locations.util.locationsConfig
@@ -20,99 +21,59 @@ class KompendiumLocationsTest : DescribeSpec({
   afterEach { Kompendium.resetSchema() }
   describe("Locations") {
     it("Can notarize a get request with a simple location") {
-      // arrange
-      withTestApplication({
+      // act
+      openApiTest("notarized_get_simple_location.json") {
         locationsConfig()
-        jacksonConfigModule()
-        docs()
         notarizedGetSimpleLocation()
-      }) {
-        // act
-        compareOpenAPISpec("notarized_get_simple_location.json")
       }
     }
     it("Can notarize a get request with a nested location") {
-      // arrange
-      withTestApplication({
+      // act
+      openApiTest("notarized_get_nested_location.json") {
         locationsConfig()
-        jacksonConfigModule()
-        docs()
         notarizedGetNestedLocation()
-      }) {
-        // act
-        compareOpenAPISpec("notarized_get_nested_location.json")
       }
     }
     it("Can notarize a post with a simple location") {
-      // arrange
-      withTestApplication({
+      // act
+      openApiTest("notarized_post_simple_location.json") {
         locationsConfig()
-        jacksonConfigModule()
-        docs()
         notarizedPostSimpleLocation()
-      }) {
-        // act
-        compareOpenAPISpec("notarized_post_simple_location.json")
       }
     }
     it("Can notarize a post with a nested location") {
-      // arrange
-      withTestApplication({
+      // act
+      openApiTest("notarized_post_nested_location.json") {
         locationsConfig()
-        jacksonConfigModule()
-        docs()
         notarizedPostNestedLocation()
-      }) {
-        // act
-        compareOpenAPISpec("notarized_post_nested_location.json")
       }
     }
     it("Can notarize a put with a simple location") {
-      // arrange
-      withTestApplication({
+      // act
+      openApiTest("notarized_put_simple_location.json") {
         locationsConfig()
-        jacksonConfigModule()
-        docs()
         notarizedPutSimpleLocation()
-      }) {
-        // act
-        compareOpenAPISpec("notarized_put_simple_location.json")
       }
     }
     it("Can notarize a put with a nested location") {
-      // arrange
-      withTestApplication({
+      // act
+      openApiTest("notarized_put_nested_location.json") {
         locationsConfig()
-        jacksonConfigModule()
-        docs()
         notarizedPutNestedLocation()
-      }) {
-        // act
-        compareOpenAPISpec("notarized_put_nested_location.json")
       }
     }
     it("Can notarize a delete with a simple location") {
-      // arrange
-      withTestApplication({
+      // act
+      openApiTest("notarized_delete_simple_location.json") {
         locationsConfig()
-        jacksonConfigModule()
-        docs()
         notarizedDeleteSimpleLocation()
-      }) {
-        // act
-        compareOpenAPISpec("notarized_delete_simple_location.json")
       }
     }
     it("Can notarize a delete with a nested location") {
-      // arrange
-      withTestApplication({
+      // act
+      openApiTest("notarized_delete_nested_location.json") {
         locationsConfig()
-        jacksonConfigModule()
-        docs()
         notarizedDeleteNestedLocation()
-      }) {
-        // act
-        compareOpenAPISpec("notarized_delete_nested_location.json")
       }
     }
   }
