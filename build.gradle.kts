043186088ea0dacd6a0166e1c57d22d2484d6bfa
Ryan Brink <rbweb@pm.me>
2021-12-04 19:42:41 -0500

    drop nexus

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
   id("io.bkbn.sourdough.root") version "0.0.1-SNAPSHOT"
-  id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
   id("com.github.jakemarsden.git-hooks") version "0.0.2"
 }
 
