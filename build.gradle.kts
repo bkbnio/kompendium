10fb3d02fa282ff558be31271aa166a34923bff1
Ryan Brink <rbweb@pm.me>
2021-12-17 17:45:36 -0500

    update to published plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,14 @@
+import io.bkbn.sourdough.gradle.core.extension.SourdoughLibraryExtension
+
 plugins {
-  id("io.bkbn.sourdough.root") version "0.0.1-SNAPSHOT"
+  id("io.bkbn.sourdough.root") version "0.1.1"
   id("com.github.jakemarsden.git-hooks") version "0.0.2"
 }
 
 sourdough {
-  sonatypeBaseUrl.set("https://s01.oss.sonatype.org")
-  javaVersion.set(JavaVersion.VERSION_11)
-  compilerArgs.set(listOf("-opt-in=kotlin.RequiresOptIn"))
+  toolChainJavaVersion = JavaVersion.VERSION_17
+  jvmTarget = JavaVersion.VERSION_11.majorVersion
+  compilerArgs = listOf("-opt-in=kotlin.RequiresOptIn")
 }
 
 gitHooks {
@@ -27,5 +29,23 @@ allprojects {
       true -> baseVersion
       else -> "$baseVersion-SNAPSHOT"
     }
+  }
+}
+
+subprojects {
+  apply(plugin = "io.bkbn.sourdough.library")
+
+  configure<SourdoughLibraryExtension> {
+    githubOrg = "bkbnio"
+    githubRepo = "kompendium"
+    githubUsername = System.getenv("GITHUB_ACTOR")
+    githubToken = System.getenv("GITHUB_TOKEN")
+    libraryName = "Kompendium" // TODO Set on a per-module basis?
+    libraryDescription = "A minimally invasive OpenAPI spec generator for Ktor"
+    licenseName = "MIT License"
+    licenseUrl = "https://mit-license.org/"
+    developerId = "bkbnio"
+    developerName = "Ryan Brink"
+    developerEmail = "admin@bkbn.io"
   }
 }
