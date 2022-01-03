6d60de8c0a5e54d71c3f08ed97c20007f8fa2f9d
Ryan Brink <rbweb@pm.me>
2022-01-03 12:31:06 -0500

    ready to start my kotlinx adventure

--- kompendium-playground/build.gradle.kts
+++ kompendium-playground/build.gradle.kts
@@ -22,7 +22,18 @@ dependencies {
   implementation(group = "io.ktor", name = "ktor-locations", version = ktorVersion)
   implementation(group = "io.ktor", name = "ktor-webjars", version = ktorVersion)
 
+  // Logging
+  implementation("org.apache.logging.log4j:log4j-api-kotlin:1.1.0")
+  implementation("org.apache.logging.log4j:log4j-api:2.17.0")
+  implementation("org.apache.logging.log4j:log4j-core:2.17.0")
+  implementation("org.slf4j:slf4j-api:1.7.32")
+  implementation("org.slf4j:slf4j-simple:1.7.32")
+
+
   implementation(group = "org.jetbrains.kotlinx", "kotlinx-serialization-json", version = "1.3.1")
 
   implementation(group = "joda-time", name = "joda-time", version = "2.10.13")
+}
+repositories {
+  mavenCentral()
 }
