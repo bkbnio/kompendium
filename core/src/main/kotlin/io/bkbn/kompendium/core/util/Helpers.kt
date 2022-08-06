954deb8915d2c58b6d9aef60d4acc8eb4af97771
Ryan Brink <5607577+unredundant@users.noreply.github.com>
2022-08-06 10:40:28 -0600

    cleanup

--- core/src/main/kotlin/io/bkbn/kompendium/core/util/Helpers.kt
+++ core/src/main/kotlin/io/bkbn/kompendium/core/util/Helpers.kt
@@ -11,27 +11,7 @@ import java.util.Locale
 
 object Helpers {
 
-  private val logger = LoggerFactory.getLogger(javaClass)
-
-  const val COMPONENT_SLUG = "#/components/schemas"
-
-  val UNIT_TYPE by lazy { Unit::class.createType() }
-
-  /**
-   * Higher order function that takes a map of names to object and will log their state ahead of function invocation
-   * along with the result of the function invocation
-   */
-  fun <T> logged(functionName: String, entities: Map<String, Any>, block: () -> T): T {
-    entities.forEach { (name, entity) -> logger.debug("Ahead of $functionName invocation, $name: $entity") }
-    val result = block.invoke()
-    logger.debug("Result of $functionName invocation: $result")
-    return result
-  }
-
-  fun KClass<*>.getSimpleSlug(prop: KProperty<*>): String = when {
-    this.typeParameters.isNotEmpty() -> genericNameAdapter(this, prop)
-    else -> simpleName ?: error("Could not determine simple name for $this")
-  }
+  private const val COMPONENT_SLUG = "#/components/schemas"
 
   fun KType.getSimpleSlug(): String = when {
     this.arguments.isNotEmpty() -> genericNameAdapter(this, classifier as KClass<*>)
@@ -44,42 +24,12 @@ object Helpers {
   }
 
   /**
-   * Will build a reference slug that is useful for schema caching and references, particularly
-   * in the case of a class with type parameters
-   */
-  fun KClass<*>.getReferenceSlug(prop: KProperty<*>): String = when {
-    this.typeParameters.isNotEmpty() -> "$COMPONENT_SLUG/${genericNameAdapter(this, prop)}"
-    else -> "$COMPONENT_SLUG/${simpleName}"
-  }
-
-  /**
    * Adapts a class with type parameters into a reference friendly string
    */
-  fun genericNameAdapter(field: KClass<*>, prop: KProperty<*>): String {
-    val typeArgs = (prop.javaField?.genericType as ParameterizedType).actualTypeArguments
-    val classNames = typeArgs.map { it as Class<*> }.map { it.kotlin }.map { it.simpleName }
-    return classNames.joinToString(separator = "-", prefix = "${field.simpleName}-")
-  }
-
-  /**
-   * Adapts a class with type parameters into a reference friendly string
-   */
-  fun genericNameAdapter(type: KType, clazz: KClass<*>): String {
+  private fun genericNameAdapter(type: KType, clazz: KClass<*>): String {
     val classNames = type.arguments
       .map { it.type?.classifier as KClass<*> }
       .map { it.simpleName }
     return classNames.joinToString(separator = "-", prefix = "${clazz.simpleName}-")
-  }
-
-  fun String.capitalized() = replaceFirstChar {
-    if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString()
-  }
-
-  fun String.toNumber(): Number {
-    return try {
-      this.toInt()
-    } catch (e: NumberFormatException) {
-      this.toDouble()
-    }
   }
 }
