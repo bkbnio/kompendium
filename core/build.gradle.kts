90ef854031c42dd321fa175fa93d7df9be00e4c1
Ryan Brink <5607577+unredundant@users.noreply.github.com>
2022-08-13 10:49:18 -0600

    versions

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -20,34 +20,35 @@ sourdoughLibrary {
 dependencies {
   // VERSIONS
   val kotestVersion: String by project
+  val ktorVersion: String by project
 
   // IMPLEMENTATION
 
   api(projects.kompendiumOas)
   api(projects.kompendiumJsonSchema)
 
-  implementation("io.ktor:ktor-server-core:2.1.0")
-  implementation("io.ktor:ktor-server-cio:2.1.0")
-  implementation("io.ktor:ktor-server-html-builder:2.1.0")
-  implementation("io.ktor:ktor-server-content-negotiation:2.1.0")
-  implementation("io.ktor:ktor-serialization-kotlinx-json:2.1.0")
+  implementation("io.ktor:ktor-server-core:$ktorVersion")
+  implementation("io.ktor:ktor-server-cio:$ktorVersion")
+  implementation("io.ktor:ktor-server-html-builder:$ktorVersion")
+  implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+  implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
   implementation("ch.qos.logback:logback-classic:1.2.11")
 
   // TEST FIXTURES
 
-  testFixturesApi("io.kotest:kotest-runner-junit5-jvm:5.4.1")
-  testFixturesApi("io.kotest:kotest-assertions-core-jvm:5.4.1")
-  testFixturesApi("io.kotest:kotest-property-jvm:5.4.1")
-  testFixturesApi("io.kotest:kotest-assertions-json-jvm:5.4.1")
+  testFixturesApi("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
+  testFixturesApi("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
+  testFixturesApi("io.kotest:kotest-property-jvm:$kotestVersion")
+  testFixturesApi("io.kotest:kotest-assertions-json-jvm:$kotestVersion")
   testFixturesApi("io.kotest:kotest-assertions-ktor-jvm:4.4.3")
 
-  testFixturesApi("io.ktor:ktor-server-core:2.1.0")
-  testFixturesApi("io.ktor:ktor-server-test-host:2.1.0")
-  testFixturesApi("io.ktor:ktor-serialization:2.1.0")
-  testFixturesApi("io.ktor:ktor-serialization-jackson:2.1.0")
-  testFixturesApi("io.ktor:ktor-serialization-gson:2.1.0")
-  testFixturesApi("io.ktor:ktor-serialization-kotlinx-json:2.1.0")
-  testFixturesApi("io.ktor:ktor-server-content-negotiation:2.1.0")
+  testFixturesApi("io.ktor:ktor-server-core:$ktorVersion")
+  testFixturesApi("io.ktor:ktor-server-test-host:$ktorVersion")
+  testFixturesApi("io.ktor:ktor-serialization:$ktorVersion")
+  testFixturesApi("io.ktor:ktor-serialization-jackson:$ktorVersion")
+  testFixturesApi("io.ktor:ktor-serialization-gson:$ktorVersion")
+  testFixturesApi("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+  testFixturesApi("io.ktor:ktor-server-content-negotiation:$ktorVersion")
 
   testFixturesApi("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
 }
--- playground/build.gradle.kts
+++ playground/build.gradle.kts
@@ -17,17 +17,17 @@ dependencies {
   // Ktor
   val ktorVersion: String by project
 
-  implementation("io.ktor:ktor-server-core:2.1.0")
-  implementation("io.ktor:ktor-server-netty:2.1.0")
-  implementation("io.ktor:ktor-server-auth:2.1.0")
-  implementation("io.ktor:ktor-server-auth-jwt:2.1.0")
-  implementation("io.ktor:ktor-serialization:2.1.0")
-  implementation("io.ktor:ktor-server-status-pages:2.1.0")
-  implementation("io.ktor:ktor-server-content-negotiation:2.1.0")
-  implementation("io.ktor:ktor-serialization-kotlinx-json:2.1.0")
-  implementation("io.ktor:ktor-serialization-jackson:2.1.0")
-  implementation("io.ktor:ktor-serialization-gson:2.1.0")
-  implementation("io.ktor:ktor-server-locations:2.1.0")
+  implementation("io.ktor:ktor-server-core:$ktorVersion")
+  implementation("io.ktor:ktor-server-netty:$ktorVersion")
+  implementation("io.ktor:ktor-server-auth:$ktorVersion")
+  implementation("io.ktor:ktor-server-auth-jwt:$ktorVersion")
+  implementation("io.ktor:ktor-serialization:$ktorVersion")
+  implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
+  implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+  implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+  implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
+  implementation("io.ktor:ktor-serialization-gson:$ktorVersion")
+  implementation("io.ktor:ktor-server-locations:$ktorVersion")
 
   // Logging
   implementation("org.apache.logging.log4j:log4j-api-kotlin:1.2.0")
