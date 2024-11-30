e9bb5a66c8c4f7ae9af9a4bc7ace4b2593268820
Michael Rittmeister <michael@rittmeister.in>
2024-11-30 22:58:08 +0100

    Fix missing types for security schemes

--- oas/src/main/kotlin/io/bkbn/kompendium/oas/security/BearerAuth.kt
+++ oas/src/main/kotlin/io/bkbn/kompendium/oas/security/BearerAuth.kt
@@ -1,9 +1,14 @@
 package io.bkbn.kompendium.oas.security
 
+import kotlinx.serialization.EncodeDefault
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
 
+@OptIn(ExperimentalSerializationApi::class)
 @Serializable
 data class BearerAuth(val bearerFormat: String? = null) : SecuritySchema {
+  @EncodeDefault(EncodeDefault.Mode.ALWAYS)
   val type: String = "http"
+  @EncodeDefault(EncodeDefault.Mode.ALWAYS)
   val scheme: String = "bearer"
 }
